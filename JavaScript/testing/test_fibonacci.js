a42aa85d2d451c5df04279650bbe4a1814cb1c88
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2022-09-07 13:47:26 -0400

    Added testing example

--- JavaScript/testing/test_fibonacci.js
+++ JavaScript/testing/test_fibonacci.js
@@ -1,10 +1,27 @@
 var assert = require('assert');
 var brokenFib = require('./brokenFibonacci')
+
+
+
+
+// describe():  Groups tests 
+// Arguments
+//    - Test group name
+//    - Callback function
+
+// it(): Defines a test case (e.g., “It should equal X” or “It should log the user in.”)
+// Arguments
+//    - Test description
+//    - Callback function containing test.
+
+
 describe('fibonacci(n)', function() {
-  it('should return 0 when the value is 0', function() {
-    assert.equal(brokenFib.fibonacci(0), 0);
-  });
-  it('should return 1 when the value is 1', function() {
-    assert.equal(brokenFib.fibonacci(1), 1);
+  describe('Base recursion cases', function() {
+    it('should return 0 when the value is 0', function() {
+      assert.equal(brokenFib.fibonacci(0), 0);
+    });
+    it('should return 1 when the value is 1', function() {
+      assert.equal(brokenFib.fibonacci(1), 1);
+    });
   });
 });  
