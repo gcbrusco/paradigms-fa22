96cf68063ed41dc4667370656c7d036d9efb02c4
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2022-10-26 15:26:49 -0400

    Live demo

--- Python/Django/live_mysite/polls/models.py
+++ Python/Django/live_mysite/polls/models.py
@@ -4,6 +4,10 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return str(self.id) + " " + self.question_text
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
--- Python/Django/mysite/polls/views.py
+++ Python/Django/mysite/polls/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
-
 from .models import Choice, Question
 
 
