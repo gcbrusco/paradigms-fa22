64205ff82b3191d0087823e8d2c905f8cdbbad6d
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2022-11-04 14:33:54 -0400

    CSV file parsing'

--- Python/REST/live_rest_client.py
+++ Python/REST/live_rest_client.py
@@ -5,10 +5,25 @@ HOST = "http://localhost:8000"
 
 
 def main():
-	# makes HTTP GET request
-	response = requests.get(f"{HOST}/bugs")
-	# parses the response (.content vs .text vs .json())
-	json_resp = response.json()
+
+	with open("bugs.csv", "w", newline="") as csvfile:
+		csv_writer = csv.writer(csvfile, delimiter=",", quotechar= '"')
+		csv_writer.writerow(["id","status","package", "summary"])
+
+		next_url = f"{HOST}/bugs"
+
+		while next_url:
+			# makes HTTP GET request
+			response = requests.get(next_url)
+			# parses the response (.content vs .text vs .json())
+			json_resp = response.json()
+			
+			for bug in json_resp["results"]:
+				csv_writer.writerow([bug["id"], bug["status"], bug["package"], bug["summary"]])
+
+			next_url = json_resp["next"]
+
+
 	# TODO: parse bugs 
 
 if __name__ == '__main__':
